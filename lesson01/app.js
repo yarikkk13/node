dad67b54aaa178b74d50db25fbd49a0dbdbea117
yarikkk13 <yaroslav.mahas@gmail.com>
2021-08-18 16:49:54 +0300

    'creating' cycles

--- lesson01/app.js
+++ lesson01/app.js
@@ -11,23 +11,43 @@ console.log(__dirname)
 // //cycle for creating boys
 // for (let num = 0; num < 10; num++) {
 //     const filePath = path.join(__dirname, 'boyz', `${num}.json`);
-//     fs.writeFile(filePath, `{"name":"max${num}","gender":"male"}`, err => {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         console.log('OK');
-//     });
+//     if (num % 2 === 0) {
+//         fs.writeFile(filePath, `{"name":"max${num}","gender":"female"}`, err => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log('OK');
+//         });
+//     } else {
+//         fs.writeFile(filePath, `{"name":"max${num}","gender":"male"}`, err => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log('OK');
+//         });
+//     }
 // }
 
 // //cycle for creating girls
 // for (let num = 10; num < 20; num++) {
 //     const filePath = path.join(__dirname, 'girlz', `${num}.json`);
-//     fs.writeFile(filePath, `{"name":"karina${num}","gender":"female"}`, err => {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         console.log('OK');
-//     });
+//     if (num % 2 === 0) {
+//         fs.writeFile(filePath, `{"name":"karina${num}","gender":"male"}`, err => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log('OK');
+//         });
+//     } else {
+//         fs.writeFile(filePath, `{"name":"karina${num}","gender":"female"}`, err => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log('OK');
+//         });
+//     }
 // }
