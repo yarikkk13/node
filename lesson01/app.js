bbbb8c12c6a59f02159563977fa16d532d779fa9
yarikkk13 <yaroslav.mahas@gmail.com>
2021-08-19 15:35:55 +0300

    jobIsDone

--- lesson01/app.js
+++ lesson01/app.js
@@ -5,68 +5,23 @@ const girlz = path.join(__dirname, 'girlz')
 const boyz = path.join(__dirname, 'boyz')
 
 
-// uncomment this chunk of code if you want create users
-// cycle for creating boys
-for (let num = 0; num < 10; num++) {
-    const filePath = path.join(boyz, `max${num}.json`);
-    if (num % 2 === 0) {
-        fs.writeFile(filePath, `{"name":"max${num}","gender":"female"}`, err => {
-            if (err) {
-                console.log(err);
-                return;
+async function movingUsers(thisPath, newPath, gender) {
+    try {
+        //get the array of files in this directory
+        const data = await readDir(thisPath);
+        //get the one file from array
+        data.forEach(async file => {
+            //reading info in this file
+            const user = JSON.parse(await readFile(path.join(thisPath, file)));
+            //comparing data's gender in file and gender
+            if (user.gender === gender) {
+                await rename((path.join(thisPath, file)), path.join(newPath, file));
             }
-            console.log('OK');
-        });
-    } else {
-        fs.writeFile(filePath, `{"name":"max${num}","gender":"male"}`, err => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log('OK');
-        });
-    }
-}
-//cycle for creating girls
-for (let num = 10; num < 20; num++) {
-    const filePath = path.join(girlz, `karina${num}.json`);
-    if (num % 2 === 0) {
-        fs.writeFile(filePath, `{"name":"karina${num}","gender":"male"}`, err => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log('OK');
-        });
-    } else {
-        fs.writeFile(filePath, `{"name":"karina${num}","gender":"female"}`, err => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log('OK');
-        });
+        })
+    } catch (err) {
+        console.warn('Something went wrong', err);
     }
 }
 
-
-// async function movingUsers(thisPath, newPath, gender) {
-//     try {
-//         //get the array of files in this directory
-//         const data = await readDir(thisPath);
-//         //get the one file from array
-//         data.forEach(async file => {
-//             //reading info in this file
-//             const user = JSON.parse(await readFile(path.join(thisPath, file)));
-//             //comparing data's gender in file and gender
-//             if (user.gender === gender) {
-//                 await rename((path.join(thisPath, file)), path.join(newPath, file));
-//             }
-//         })
-//     } catch (err) {
-//         console.warn('Something went wrong', err);
-//     }
-// }
-//
-// movingUsers(girlz, boyz, "male")
-// movingUsers(boyz, girlz, "female")
+movingUsers(girlz, boyz, "male")
+movingUsers(boyz, girlz, "female")
